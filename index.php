170447360ca1dd9bb30d8c4f3881f5635d603cd6
arumu <albert.rumu@gmail.com>
2017-04-02 11:50:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,18 @@
 <html>
 <head>
 	<title>Convertir grados</title>
-        <link rel="stylesheet" type="text/css" href="estilos.css"/>        
-
+        <link rel="stylesheet" type="text/css" href="estilos.css"/>
 </head>
+	
 <body>
-
     <div><h2>Conversor grados Celsius/Fahrenheit</h2></div>
     <div >
- <form action="convierte.php" method="post">
-     <div> <label>Introduzca valor a convertir: </label></div>
-  <input class="texto" type="text"  name="numero"/><br/>
-  <input class="btn" type="submit" name="celsius" value="celsius"/>
-  <input class="btn" type="submit" name="fahrenheit" value="fahrenheit" />
- </form>
+      <form action="convierte.php" method="post">
+       <div> <label>Introduzca valor a convertir: </label></div>
+       <input class="texto" type="text"  name="numero"/><br/>
+       <input class="btn" type="submit" name="celsius" value="celsius"/>
+       <input class="btn" type="submit" name="fahrenheit" value="fahrenheit" />
+      </form>
     </div>
 </body>
 </html>
